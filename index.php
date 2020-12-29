412190ced0fe91067c7a5753c63dba45056cc7b1
WolFWarrioR (Vadim Baranovsky) <b07vadim@gmail.com>
2020-12-29 21:20:39 +0200

    [+] Google Tag Manager

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N7KXVP5');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
           name="viewport">
@@ -27,6 +34,10 @@
         ChatbroLoader({encodedChatId: '67J2q'});</script> <!-- chatBro -->
 </head>
 <body class="bgWrapper" style="/*background-image: url(https://hot-game.news/wp-content/uploads/2020/11/bg-5.jpg)*/">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7KXVP5"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header style="animation-name:fadeInDown">
     <div class="logoWrapper">
         <a href="#">
